85363187ff09ae87b2ab3b9ac1673d89e3b4eab7
Venkatesh-boot <venkatesh.net5@gmail.com>
2025-12-05 15:04:57 +0530

    Fix inner function to return sum when no argument is provided for chaining

--- javascript/infiniteClosure.js
+++ javascript/infiniteClosure.js
@@ -2,9 +2,14 @@ function add(a) {
   let sum = a;
 
   function inner(b) {
+    if (b === undefined) {
+      return sum;
+    }
     sum += b;
     return inner;        // return itself for chaining
   }
 
   return inner;
 }
+
+console.log(add(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)()); // Outputs: 55
