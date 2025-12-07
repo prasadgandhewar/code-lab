cdf2f2e715be3c71f7546817a166f5462fbc457e
Venkatesh-boot <venkatesh.net5@gmail.com>
2025-12-07 11:31:54 +0530

    Implement debounce function to limit the rate of function execution

---
+++ javascript/debounce.js
@@ -0,0 +1,21 @@
+function debounce(fn, delay){
+    let timerid = null;
+    return function(...args){
+        if(timerid){
+            clearTimeout(timerid);
+        }
+        
+        timerid = setTimeout(()=>{
+            fn.apply(null, ...args);
+        }, delay);
+    }
+}
+
+function myfunc(){
+    console.log('hi');
+}
+
+const defun = debounce(myfunc, 1000);
+defun();
+defun('a');
+defun('aa');
